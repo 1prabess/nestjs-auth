40bd85f56d556ecaf292cd22e854f3d9bdc8fa4c
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-01-22 14:32:12 +0545

    Create login service

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UserService } from 'src/user/providers/user.service';
+import { HashingProvider } from './hashing.provider';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/user/schema/user.schema';
+import type { Response } from 'express';
+import { TokenPayload } from '../interfaces/token-payload.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(forwardRef(() => UserService))
+    private readonly userService: UserService,
+    private readonly hashingProvider: HashingProvider,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: User, response: Response) {
+    const accessTokenCookieExpiry = new Date();
+    accessTokenCookieExpiry.setTime(
+      accessTokenCookieExpiry.getTime() +
+        Number(this.configService.getOrThrow('JWT_ACCESS_TOKEN_EXPIRATION_MS')),
+    );
+
+    const payload: TokenPayload = {
+      userId: user._id.toHexString(),
+    };
+
+    const accessToken = this.jwtService.sign(payload, {
+      secret: this.configService.getOrThrow('JWT_ACCESS_TOKEN_SECRET'),
+      expiresIn: `${this.configService.getOrThrow('JWT_ACCESS_TOKEN_EXPIRATION_MS')}ms`,
+    });
+
+    return { accessToken };
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await this.hashingProvider.compare(
+      password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return user;
+  }
+}
