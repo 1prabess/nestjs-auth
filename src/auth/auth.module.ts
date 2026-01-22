87e8e3b336507d01805632839f8e3ac62f4b9671
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-01-22 14:31:38 +0545

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,27 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './providers/auth.service';
 import { HashingProvider } from './providers/hashing.provider';
 import { BcryptProvider } from './providers/bcrypt.provider';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
+  imports: [forwardRef(() => UserModule), PassportModule, JwtModule],
   exports: [HashingProvider],
   providers: [
     AuthService,
+    LocalStrategy,
+    JwtStrategy,
     {
       provide: HashingProvider,
       useClass: BcryptProvider,
     },
     BcryptProvider,
   ],
+  controllers: [AuthController],
 })
 export class AuthModule {}
