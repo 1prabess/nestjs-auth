bda7a32ad3b224b56c857ffd9917c19256b25a8e
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-01-22 14:32:51 +0545

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from 'src/dtos/create-user.dto';
 import { UserService } from './providers/user.service';
 import { ApiOperation } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { User } from './schema/user.schema';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('register')
-  @ApiOperation({ summary: 'Register a new user' })
+  @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async findAll(@CurrentUser() user: User) {
+    console.log(user);
+    return this.userService.findAll();
   }
 }
