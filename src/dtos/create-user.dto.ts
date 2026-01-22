5ecfc6bdfd1521d2904f3fd240dd7de61700b660
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-01-22 10:51:12 +0545

    Create create-user.dto.ts

---
+++ src/dtos/create-user.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    description: 'Email of the user',
+    example: 'user@example.com',
+  })
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    description:
+      'Password of the user. Must be strong (min 8 chars, uppercase, lowercase, number, symbol)',
+    example: 'StrongP@ssw0rd!',
+  })
+  @IsNotEmpty()
+  @IsStrongPassword()
+  password: string;
+}
