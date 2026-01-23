3e7ae18ca39059eb4c23e26f5d824d4a14452fed
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-01-23 11:07:41 +0545

    Update user.service.ts

--- src/user/providers/user.service.ts
+++ src/user/providers/user.service.ts
@@ -1,8 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from '../schema/user.schema';
 import { Model } from 'mongoose';
-import { CreateUserDto } from 'src/dtos/create-user.dto';
 import { HashingProvider } from 'src/auth/providers/hashing.provider';
 
 @Injectable()
@@ -13,17 +12,25 @@ export class UserService {
     private readonly hashingProvider: HashingProvider,
   ) {}
 
-  async create(createUserDto: CreateUserDto) {
+  async create(email: string, password: string) {
+    const existingUser = await this.findByEmail(email);
+
+    if (existingUser) {
+      throw new UnauthorizedException('User already exists');
+    }
+
+    const hashedPassword = await this.hashingProvider.hash(password);
+
     const user = new this.userModel({
-      ...createUserDto,
-      password: await this.hashingProvider.hash(createUserDto.password),
+      email,
+      password: hashedPassword,
     });
 
     return await user.save();
   }
 
-  async findById(id: string) {
-    return await this.userModel.findById(id);
+  async findById(userId: string) {
+    return await this.userModel.findById(userId);
   }
 
   async findByEmail(email: string) {
@@ -31,6 +38,22 @@ export class UserService {
   }
 
   async findAll() {
-    return await this.userModel.find();
+    return this.userModel.find().select('-password -refreshToken');
+  }
+
+  async setRefreshToken(userId: string, refreshToken: string) {
+    const hashedRefreshToken = await this.hashingProvider.hash(refreshToken);
+
+    await this.userModel.findByIdAndUpdate(userId, {
+      refreshToken: hashedRefreshToken,
+    });
+  }
+
+  async findByIdWithRefreshToken(userId: string) {
+    return await this.userModel.findById(userId).select('+refreshToken');
+  }
+
+  async removeRefreshToken(userId: string) {
+    await this.userModel.findByIdAndUpdate(userId, { refreshToken: null });
   }
 }
