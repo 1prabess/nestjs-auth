85319793d7e88e0dde8d36f6d253952b0335379e
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-01-23 11:06:27 +0545

    Create auth guards

---
+++ src/auth/guards/role.guard.ts
@@ -0,0 +1,39 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { ROLES_KEY } from 'src/common/decorators/role.decorator';
+import { User } from 'src/user/schema/user.schema';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user: User = request.user;
+
+    if (!requiredRoles.includes(user.role)) {
+      throw new ForbiddenException(
+        `Access denied: requires one of [${requiredRoles.join(', ')}] role`,
+      );
+    }
+
+    return true;
+  }
+}
